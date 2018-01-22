7e862cc23ec51dd2b26c2e96ebb8f0decf31a223
Mardzy <paul.mardling@itv.com>
2018-01-22 15:55:49 +0000

    updated package.json

--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -3,14 +3,14 @@ const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
   const imageTitle = video.snippet.title;
   return (
-    <li onClick={()=> onVideoSelect(video)} className="list-group-item">
-      <div className="video-list media">
-        <div className="media-left">
-          <img className="media-object" src={imageUrl}/>
+    <li onClick={()=> onVideoSelect(video)} className='list-group-item'>
+      <div className='video-list media'>
+        <div className='media-left'>
+          <img className='media-object' src={imageUrl} />
         </div>
       </div>
-      <div className="media-body">
-        <div className="media-heading">
+      <div className='media-body'>
+        <div className='media-heading'>
           {imageTitle}
         </div>
       </div>
